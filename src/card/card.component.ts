b729115b739155aaf70ed1eebe99f9006de1ccdb
Sergey <sergey@malyuga.ru>
2025-08-14 11:09:48 +0300

    Добавить функционал добавления оффера в избранное

--- src/card/card.component.ts
+++ src/card/card.component.ts
@@ -1,4 +1,13 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output, signal} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  signal,
+  ViewChild
+} from '@angular/core';
 import {OfferPreview} from '../types/offers';
 import {CapitalizePipe} from './capitalize.pipe';
 import {RouterModule} from '@angular/router';
@@ -15,19 +24,27 @@ export class CardComponent {
 
   protected readonly Math = Math;
   protected readonly AppRoute = AppRoute;
+
   @Input() offer: OfferPreview | undefined = undefined;
   @Input() isFavoritePage = false;
   @Input() isOfferPage = false;
-  isActive = signal(false);
 
   @Output() cardMouseEnter = new EventEmitter<OfferPreview>();
   @Output() cardMouseLeave = new EventEmitter<void>();
 
+  @ViewChild('favoriteButton') favoriteButton!: ElementRef;
+
   onMouseEnter() {
     this.cardMouseEnter.emit(this.offer);
   }
+
   onMouseLeave = () => {
-   this.cardMouseLeave.emit();
+    this.cardMouseLeave.emit();
+  }
+
+  toggleOfferFavorite() {
+    const target = this.favoriteButton.nativeElement as HTMLElement
+    target.classList.toggle('place-card__bookmark-button--active');
   }
 }
 
